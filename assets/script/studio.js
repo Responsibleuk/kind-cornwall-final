0d1f9ebf6d4e34fd548918081a48739b51fb6671
Responsibleuk <james@we-are-responsible.com>
2022-01-04 14:53:44 +0000

    Add studios page, add to nav bar on index.html, add structure to studios, add styling to studios, add studio.js and add accordian code

---
+++ assets/script/studio.js
@@ -0,0 +1,7 @@
+const accordion_items_elms = document.querySelectorAll('.accordion .accordion-item');
+accordion_items_elms.forEach(accordionItem => {
+  accordionItem.querySelector('.accordion-item-head').addEventListener('click', (e) => {
+    accordionItem.classList.toggle('open');
+  });
+
+});
