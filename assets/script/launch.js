c317f691b0eeae818013cb1670c5680ba44a56f9
Responsibleuk <james@we-are-responsible.com>
2022-01-04 11:43:52 +0000

    Add countdown timer, add styling to countdown, add geometric heart, add countdown script

---
+++ assets/script/launch.js
@@ -0,0 +1,88 @@
+// Countdown
+
+const countdownDescription = document.querySelector ('section-center');
+
+const months = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December',
+  ];
+  const weekdays = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const giveaway = document.querySelector('.date');
+  const deadline = document.querySelector('.launch');
+  const items = document.querySelectorAll('.launch-format h4');
+  
+  let tempDate = new Date();
+  let tempYear = tempDate.getFullYear();
+  let tempMonth = tempDate.getMonth();
+  let tempDay = tempDate.getDate();
+ 
+  let futureDate = new Date(2022, 3, 25, 9, 30, 0);
+  
+  const year = futureDate.getFullYear();
+  const hours = futureDate.getHours();
+  const minutes = futureDate.getMinutes();
+  
+  let month = futureDate.getMonth();
+  month = months[month];
+  const weekday = weekdays[futureDate.getDay()];
+  const date = futureDate.getDate();
+  giveaway.textContent = `Kind Cornwall Launches ${weekday}, ${date} ${month} ${year} ${hours}:${minutes}am`;
+  
+  const futureTime = futureDate.getTime();
+  function getRemaindingTime() {
+    const today = new Date().getTime();
+  
+    const t = futureTime - today;
+   
+    const oneDay = 24 * 60 * 60 * 1000;
+    const oneHour = 60 * 60 * 1000;
+    const oneMinute = 60 * 1000;
+    // calculate all values
+    let days = t / oneDay;
+    days = Math.floor(days);
+    let hours = Math.floor((t % oneDay) / oneHour);
+    let minutes = Math.floor((t % oneHour) / oneMinute);
+    let seconds = Math.floor((t % oneMinute) / 1000);
+  
+    // set values array
+    const values = [days, hours, minutes, seconds];
+    function format(item) {
+      if (item < 10) {
+        return (item = `0${item}`);
+      }
+      return item;
+    }
+  
+    items.forEach(function (item, index) {
+      item.innerHTML = format(values[index]);
+    });
+  
+    if (t < 0) {
+      clearInterval(countdown);
+      deadline.innerHTML = `<h4 class="expired">Kind Cornwall has launched, membership giveaway has ended</h4>`;
+    }
+  }
+  // countdown;
+  let countdown = setInterval(getRemaindingTime, 1000);
+  //set initial values
+  getRemaindingTime();
+
+
